7b2e56d6bc5734b4d8ba4b047d6010765ec88508
Carlos Matheu Armengol <info@plastikaweb.com>
2016-09-20 17:40:34 +0200

    add hero class

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,18 @@
-// Every Angular application has at least one component:
-// the root component, named AppComponent here.
-//
-// Components are the basic building blocks of Angular applications.
-// A component controls a portion of the screen—a view—through its associated template.
-import { Component }         from '@angular/core';
-
+import {Component}         from '@angular/core';
+export class Hero {
+    id: number;
+    name: string;
+}
 @Component({
     selector: 'my-app',
-    template: '<h1>My first Angular 2 App</h1>'
+    template: '<h1>{{ title }}</h1>' +
+    '<h2>{{ hero.name }} details!</h2>'
 })
-export class AppComponent { }
+
+export class AppComponent {
+    title = 'Tour of Heroes';
+    hero: Hero = {
+        id: 1,
+        name: 'Widnstorm'
+    }
+}
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,16 +1,3 @@
-// This is the entry point to the application
-//
-// Since the QuickStart application is a web application that runs in a browser,
-// your root module needs to import the BrowserModule
-// from @angular/platform-browser to the imports array.
-//
-// This is the smallest amount of Angular that is needed
-// for a minimal application to run in the browser.
-//
-// The QuickStart application doesn't do anything else,
-// so you don't need any other modules.
-// In a real application, you'd likely import FormsModule as well
-// as RouterModule and HttpModule.
 import { NgModule }         from '@angular/core';
 import { BrowserModule }    from '@angular/platform-browser';
 import { AppComponent }     from './app.component'
