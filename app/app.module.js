0358a51b453158a577155301030c226fb0228bf6
Carlos Matheu Armengol <info@plastikaweb.com>
2016-09-20 16:22:15 +0200

    create app.module

---
+++ app/app.module.js
@@ -0,0 +1,38 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+// This is the entry point to the application
+//
+// Since the QuickStart application is a web application that runs in a browser,
+// your root module needs to import the BrowserModule
+// from @angular/platform-browser to the imports array.
+//
+// This is the smallest amount of Angular that is needed
+// for a minimal application to run in the browser.
+//
+// The QuickStart application doesn't do anything else,
+// so you don't need any other modules.
+// In a real application, you'd likely import FormsModule as well
+// as RouterModule and HttpModule.
+var core_1 = require('@angular/core');
+var platform_browser_1 = require('@angular/platform-browser');
+var AppModule = (function () {
+    function AppModule() {
+    }
+    AppModule = __decorate([
+        core_1.NgModule({
+            imports: [platform_browser_1.BrowserModule]
+        }), 
+        __metadata('design:paramtypes', [])
+    ], AppModule);
+    return AppModule;
+}());
+exports.AppModule = AppModule;
+//# sourceMappingURL=app.module.js.map
