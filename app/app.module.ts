aa6c7679abf8078076b77d931e199191d363ef22
Carlos Matheu Armengol <info@plastikaweb.com>
2016-09-21 20:52:41 +0200

    heroes routing

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,12 +1,16 @@
-import { NgModule }         from '@angular/core';
-import { BrowserModule }    from '@angular/platform-browser';
-import { FormsModule }      from '@angular/forms';
-import { AppComponent }     from './app.component'
-import {HeroDetailComponent} from "./hero-detail.component";
+import { NgModule }             from '@angular/core';
+import { BrowserModule }        from '@angular/platform-browser';
+import { FormsModule }          from '@angular/forms';
+import { HeroesComponent }      from './heroes.component';
+import { AppComponent }         from './app.component';
+import { HeroDetailComponent }  from "./hero-detail.component";
+import { HeroService }          from "./hero.service";
+import { routing }                from "./app.routing";
 
 @NgModule({
-    imports:        [ BrowserModule, FormsModule ],
-    declarations:   [ AppComponent, HeroDetailComponent ],
+    imports:        [ BrowserModule, FormsModule, routing ],
+    declarations:   [ AppComponent, HeroesComponent, HeroDetailComponent ],
+    providers:      [ HeroService ],
     bootstrap:      [ AppComponent ]
 })
 export class AppModule { }
