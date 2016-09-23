a2733b32baabd5925746d294831a75f5e4fe4efc
Carlos Matheu Armengol <info@plastikaweb.com>
2016-09-23 11:59:55 +0200

    delete Hero

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -45,6 +45,14 @@ export class HeroService {
             .catch(this.handleError);
     }
 
+    delete(heroId: number): Promise<void> {
+        let url = `${this.heroesUrl}/${heroId}`;
+        return this.http.delete(url, {headers: this.headers})
+            .toPromise()
+            .then(() => null)
+            .catch(this.handleError);
+    }
+
     private handleError(error: any): Promise<any> {
         console.error('An error occurred', error);
         return Promise.reject(error.message || error);
